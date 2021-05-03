7927ab1d623fa324cd9ad7b4fc2a9c90e76df8fd
mena-hub <mssz.nnia@gmail.com>
2021-05-03 15:18:21 -0300

    Register model with the admin

--- bigbox/admin.py
+++ bigbox/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Reason, Category, Activity, Box
 
-# Register your models here.
+admin.site.register(Reason)
+admin.site.register(Category)
+admin.site.register(Activity)
+admin.site.register(Box)
