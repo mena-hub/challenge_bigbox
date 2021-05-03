af4ccb186dfc60cecdb4e97bd8159167e2cea1c9
mena-hub <mssz.nnia@gmail.com>
2021-05-03 15:10:33 -0300

    Run migrations

--- bigbox/models.py
+++ bigbox/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=200, verbose_name='nombre')
+    slug = models.SlugField()
+    order = models.IntegerField(default=0, verbose_name=u'orden')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+class Reason(CommonInfo):  
+    pass 
+    
+class Category(CommonInfo):    
+    description = models.TextField(verbose_name=u'descripción')
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    internal_name = models.CharField(max_length=200)
+    description = models.TextField(verbose_name=u'descripción')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True, verbose_name='categoría')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+class Activity(Product):
+    reasons = models.ManyToManyField(Reason, blank=True, verbose_name='tags')
+    purchase_available = models.BooleanField(default=False, verbose_name='disponible venta individual')
+
+class Box(Product):
+    activities = models.ManyToManyField(Activity)
+    price = models.IntegerField(verbose_name='precio de venta')
+    purchase_available = models.BooleanField(default=False, verbose_name='disponible venta individual')
+    # slug = models.CharField(max_length = 20, null=True)
